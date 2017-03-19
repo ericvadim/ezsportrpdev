789911d15c67981dc039561bcca421f4ab3f423e
ericvadim <ericvadim@mail.com>
2017-03-19 18:44:11 +0100

    referee grades page

--- app/app.js
+++ app/app.js
@@ -84,8 +84,8 @@ angular.module('app', [
 
     })
     .constant('APP_CONFIG', window.appConfig)
-    // .constant('ServerURL', 'http://ezsportrp.info/server/')
-    .constant('ServerURL', 'http://localhost/ezsportrp/server/')
+    .constant('ServerURL', 'http://ezsportrp.info/server/')
+    // .constant('ServerURL', 'http://localhost/ezsportrp/server/')
 
     .run(function ($rootScope
         , $state, $stateParams) {
