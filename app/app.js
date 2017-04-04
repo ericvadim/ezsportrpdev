a60f91f8e9240a85ee1e186b098a1e7ea03c6a93
ericvadim <ericvadim@mail.com>
2017-04-04 19:33:12 +0200

    done person management

--- app/app.js
+++ app/app.js
@@ -343,6 +343,6 @@ angular.module('app', [
     .constant('UserTypes', ['Normal', 'Player', 'Coach', 'Referee', 'Administrator'])
     .constant('CoachTypes', ['Head Coach', 'Assistance Coach', 'Trainer', 'Goal Keeper Coach'])
     .constant('SeasonList', ['Spring', 'Summer', 'Winter'])
-    // .constant('ServerURL', 'http://ezsportrp.info/server/')
-    .constant('ServerURL', 'http://localhost/ezsportrp/server/')
+    .constant('ServerURL', 'http://ezsportrp.info/server/')
+    // .constant('ServerURL', 'http://localhost/ezsportrp/server/')
 ;
