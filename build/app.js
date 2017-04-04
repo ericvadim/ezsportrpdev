45d0657f1f26a8415391f19a76d87af2643dcd45
ericvadim <ericvadim@mail.com>
2017-04-04 21:23:44 +0200

    done adding group level in league managment page

--- build/app.js
+++ build/app.js
@@ -634,8 +634,8 @@ angular.module('app', [
     .constant('CoachTypes', ['Head Coach', 'Assistance Coach', 'Trainer', 'Goal Keeper Coach'])
     .constant('SeasonList', ['Spring', 'Summer', 'Winter'])
     .constant('GroupLevels', {1: 'Bronze', 2: 'Silver', 3: 'Gold', 4: 'State', 5: 'State Premier', 6: 'National Premier'})
-    // .constant('ServerURL', 'http://ezsportrp.info/server/')
-    .constant('ServerURL', 'http://localhost/ezsportrp/server/')
+    .constant('ServerURL', 'http://ezsportrp.info/server/')
+    // .constant('ServerURL', 'http://localhost/ezsportrp/server/')
 ;
 "use strict";
 
