5f4e6dd8234be4324e90324eb137dc8a17b17437
ericvadim <ericvadim@mail.com>
2017-04-27 06:29:31 +0200

    team management

--- server/application/models/Team_model.php
+++ server/application/models/Team_model.php
@@ -16,7 +16,7 @@ class Team_model extends CI_Model
         $rows = $this->db->get_where($this->table, array('club_id' => $clubId))->result();
         if (sizeof($rows)) {
             foreach ($rows as $key => $value) {
-                $image = $this->imagePath . $value->id . '.jpg';
+                $image = base_url() . $this->imagePath . $value->id . '.jpg';
                 $value->image = file_exists($image) ? base_url() . $image : './styles/img/no.jpg';
             }
         }
@@ -58,7 +58,7 @@ class Team_model extends CI_Model
 
     public function deleteRowById($rowId)
     {
-        $file = $this->imagePath . $rowId . '.jpg';
+        $file = base_url() . $this->imagePath . $rowId . '.jpg';
         if (file_exists($file)) unlink($file);
         return $this->db->delete($this->table, array('id' => $rowId));
     }
