d5fb5bffcc9400526d9142d3bdfe354f8e769140
ericvadim <ericvadim@mail.com>
2017-05-16 06:00:32 +0200

    dashboard

--- app/admin/controllers/DashboardCtrl.js
+++ app/admin/controllers/DashboardCtrl.js
@@ -2,5 +2,7 @@
 
 angular.module('app.admin').controller('DashboardController', function ($scope) {
 
-
+    $scope.teamData = [1,2,3,4,5];
+    $scope.playerData = [1,2,3,4,5];
+    $scope.scheduleData = [1,2,3,4,5];
 });
