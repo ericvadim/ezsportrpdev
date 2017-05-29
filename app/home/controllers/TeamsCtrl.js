91228368c7640912978d0c0580ebc7d126a5d178
ericvadim <ericvadim@mail.com>
2017-05-30 01:27:16 +0200

    teams page

--- app/home/controllers/TeamsCtrl.js
+++ app/home/controllers/TeamsCtrl.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('app.home').controller('HomeTeamsController', function ($scope) {
-
-
+    $scope.scheduleData = [1, 2, 3, 4, 5];
+    $scope.recordItems = ['Goal', 'Assist', 'Passes', 'Tackles', 'Saves'];
+    $scope.currRecordItem = {
+        name: $scope.recordItems[0]
+    };
 });
